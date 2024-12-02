ba6de4a9810b1cd5c571248f52ed9a02ff99e1a7
Bo Ying Wang <1546298@gmail.com>
2024-12-02 23:29:42 +0800

    feat: use API to reserve

--- get_field.py
+++ get_field.py
@@ -14,6 +14,8 @@ import requests
 from io import BytesIO
 import base64
 import time
+import datetime
+import json
 
 
 class Get_field():
@@ -55,18 +57,21 @@ class Get_field():
         # 登入
         self.driver.find_elements(By.CLASS_NAME, "btn-login")[0].click()
 
+        self.cookie = self.driver.get_cookies()[0]['value']
+
         return self.driver
 
     def open_url(self, URL):
         # 安裝及啟動Chrome瀏覽器
         self.driver = webdriver.Chrome()
         self.driver.get(URL)
-
+        print("ASDASD")
         return self.driver
 
-    def get_availble_field(self, weekday, time):
+    def get_available_field(self, weekday, time):
         print("get_availble_field: start")
         weekday_XPath = "//*[@id='frame_2']/table/tbody/tr[1]/td/table[1]/tbody/tr/td[" + weekday + "]/div[1]"
+        self.driver.implicitly_wait(1)
         table_header = self.driver.find_elements(By.XPATH, weekday_XPath)[0]
         table_header.click()
 
@@ -87,10 +92,55 @@ class Get_field():
             if (alert.text == "預約成功"):
                 print(alert.text)
                 alert.accept()
+                print("weekdays:", weekday, "times:", time,
+                      "result: available")
                 return True
             else:
                 print(alert.text)
                 alert.accept()
-        
+
         print("No available")
         return False
+
+    def get_fieldAPI(self, reserve_year, reserve_month, reserve_day, timeID):
+        date_time = datetime.datetime(reserve_year, reserve_month, reserve_day,
+                                      0, 0)
+        timestamp = str(int(time.mktime(date_time.timetuple())))
+
+        # API URL
+        url = 'https://nthualb.url.tw/reservation/api/reserve_field'
+        cookie_str = 'PHPSESSID=' + self.cookie
+
+        for fieldID in range(0, 8):
+            # API 請求資料
+            data = {
+                'time': str(timeID),  # 替換為您需要的 timeID
+                'field': str(fieldID),  # 替換為您需要的 fieldID
+                'date': timestamp  # 替換為您需要的日期
+            }
+
+            # 進行 API 請求
+            response = requests.post(
+                url,
+                headers={
+                    'Content-Type': 'application/json',  # 確保告知伺服器傳送的是 JSON 格式
+                    'Cookie': cookie_str  # 替換為您的 PHPSESSID
+                },
+                data=json.dumps(data)  # 將 Python 字典轉為 JSON
+            )
+
+            # 處理回應
+            if response.status_code == 200:
+                print(f"Response: {response.text}")
+                if (response.text == "預約失敗，日期錯誤"):
+                    return 1
+                elif (response.text == "reserved"):
+                    continue
+                elif (response.text == "ok"):
+                    return 0
+                else:
+                    return -1
+            else:
+                print(f"Failed to call API. Status Code: {response.status_code}")
+                return -1
+        return 2
--- main.py
+++ main.py
@@ -1,46 +1,64 @@
 from get_field import Get_field
 
+from time import sleep, time
+from multiprocessing import Pool
 import os
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-#校友體育館登入網址
-# test_URL = 'C:\\Users\\bywang\\Desktop\\captcha\\god\\國立清華大學 校友體育館.html'
-URL = 'https://oauth.ccxp.nthu.edu.tw/v1.1/authorize.php?client_id=nthualb&response_type=code'
-# URL = 'https://oauth.ccxp.nthu.edu.tw/v1.1/authorize.php?response_type=code&client_id=eeclass&redirect_uri=https%3A%2F%2Feeclass.nthu.edu.tw%2Fservice%2Foauth%2F&scope=lmsid+userid&state=&ui_locales=zh-TW'
-ACCOUNT = "YOUR_ACCOUNT"
-PASSWORD = "YOUR_PASSWORD"
-MODEL_PATH = "YOUR_MODEL_PATH"
+if __name__ == '__main__':
+    #校友體育館登入網址
+    test_URL = 'C:\\Users\\bywang\\Desktop\\captcha\\god\\test.html'
+    URL = 'https://oauth.ccxp.nthu.edu.tw/v1.1/authorize.php?client_id=nthualb&response_type=code'
+    # URL = 'https://oauth.ccxp.nthu.edu.tw/v1.1/authorize.php?response_type=code&client_id=eeclass&redirect_uri=https%3A%2F%2Feeclass.nthu.edu.tw%2Fservice%2Foauth%2F&scope=lmsid+userid&state=&ui_locales=zh-TW'
+    ACCOUNT = "YOUR_ACCOUNT"
+    PASSWORD = "YOUR_PASSWORD"
+    MODEL_PATH = "YOUR_MODEL_PATH"
 
-robot = Get_field(URL, ACCOUNT, PASSWORD, MODEL_PATH)
-robot.login()
-# robot.open_url(test_URL)
-'''
-robot.get_availble_field(weekday, time)
-    weekday: 第幾個星期幾的欄位 (1~5) 5表示最新的日期，非實際星期幾
-    time: 第幾個時段 (1~9)
-    7:00~8:00	1
-    8:00~9:00	2
-    9:00~10:00	3
-    10:00~11:00	4
-    11:00~12:00	5
-    12:00~13:00	6
-    13:00~14:00	7
-    14:00~15:00	8
-    15:00~16:00	9
-    16:00~17:00	10
-    17:00~18:00	11
-    18:00~19:00	12
-    19:00~20:00	13
-    20:00~21:00	14
-    21:00~22:00	15
-    22:00~23:00	16
+    # robot.open_url(test_URL)
+    '''
+    robot.get_availble_field(weekday, time)
+        weekday: 第幾個星期幾的欄位 (1~5) 5表示最新的日期，非實際星期幾
+        time: 第幾個時段
+        7:00~8:00	0
+        8:00~9:00	1
+        9:00~10:00	2
+        10:00~11:00	3
+        11:00~12:00	4
+        12:00~13:00	5
+        13:00~14:00	6
+        14:00~15:00	7
+        15:00~16:00	8
+        16:00~17:00	9
+        17:00~18:00	10
+        18:00~19:00	11
+        19:00~20:00	12
+        20:00~21:00	13 **
+        21:00~22:00	14 **
+        22:00~23:00	15
 
-'''
-robot.get_availble_field('5', '9')
-robot.get_availble_field('5', '10')
+    '''
+    s_time = time()
+    reserve_year = 2024
+    reserve_month = 12
+    reserve_day = 7
+    times = ['13', '14']
+    results = []
 
+    robot = Get_field(URL, ACCOUNT, PASSWORD, MODEL_PATH)
+    robot.login()
+    cnt = 1
+    for t in times:
+        while True:
+            ret = robot.get_fieldAPI(reserve_year, reserve_month, reserve_day,
+                                     t)
+            if ret != 1:  # 時間還沒到
+                break
+            sleep(3)
+            print("第", cnt, "次嘗試時間未到")
+            cnt += 1
+        results.append(ret)
 
-while True:
-    pass
+    for idx, ret in enumerate(results):
+        print(f"第{idx+1}筆結果: {ret}")
